e0e7acbcce5d523654ff7c6311c38df6ed00c8e5
ekaterinautkina003 <katusha_utkina@mail.ru>
2024-02-25 21:08:31 +0300

    commit7

--- src/main/java/ru/yandex/practicum/filmorate/model/Friendship.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Friendship.java
@@ -2,9 +2,13 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
 
+import javax.validation.constraints.Pattern;
+
 @Data
 public class Friendship {
+    @Pattern(regexp = "^[a-z][a-zA-Z0-9]*$")
     private long user_id;
+    @Pattern(regexp = "^[a-z][a-zA-Z0-9]*$")
     private long friend_id;
     private FrienshipStatus status;
 }
